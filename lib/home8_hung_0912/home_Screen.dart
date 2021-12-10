233bdcb9e83b58866ec5b494be938c1c7c79bc9d
hungbap <69029818+hungbap@users.noreply.github.com>
2021-12-10 09:08:48 +0700

    update code

--- lib/home8_hung_0912/home_Screen.dart
+++ lib/home8_hung_0912/home_Screen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 class homeScreen8 extends StatefulWidget {
-  const homeScreen8({Key? key}) : super(key: key);
-
   @override
   _homeScreen8State createState() => _homeScreen8State();
 }
 
 class _homeScreen8State extends State<homeScreen8> {
   @override
+  void initState() {
+
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tìm kiếm sản phẩm',style: TextStyle(color: Colors.white),),
+        backgroundColor: Color(0xfff0886f),
+      ),
+      backgroundColor: Color(0xfff1f2f4),
+      body: Container(
+
+      ),
+    );
   }
 }
